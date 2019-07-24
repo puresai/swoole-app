1172b37910b4b3be6cb79d8ff16fe208fb270405
13sai <sai0556@qq.com>
2019-07-24 18:24:11 +0800

    wss

--- public/ChatServer.php
+++ public/ChatServer.php
@@ -8,7 +8,9 @@ $env = \Dotenv\Dotenv::create(dirname(__DIR__));
 $env->load();
 
 $options = [
-    'daemonize' => true
+    'daemonize' => true,
+    'ssl_cert_file'=> env('SSL_CERT'),
+    'ssl_key_file' =>env('SSL_KEY')
 ];
 $ws = new Chat($options, '0.0.0.0', getenv('SERVER_PORT', 9500));
 $ws->start();
